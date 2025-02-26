51cc962a25f2127c5da4c9f6fd87fe518c4b0908
Samir Bravo <sambravo@gmail.com>
2025-02-25 23:54:15 -0500

    Builder Design Pattern

---
+++ src/main/java/designPatterrns/creational/Builder/BuilderPatternExample.java
@@ -0,0 +1,18 @@
+package designPatterrns.creational.Builder;
+
+public class BuilderPatternExample {
+    public static void main(String[] args) {
+        // Creating a Car with Air Conditioning and GPS
+        Vehicle car = new Vehicle.VehicleBuilder("Car", 4)
+                .withGPS()
+                .withAirConditioning()
+                .build();
+
+        // Creating a Bike with only basic features
+        Vehicle bike = new Vehicle.VehicleBuilder("Bike", 2)
+                .build();
+
+        System.out.println(car);
+        System.out.println(bike);
+    }
+}
---
+++ src/main/java/designPatterrns/creational/Builder/Vehicle.java
@@ -0,0 +1,52 @@
+package designPatterrns.creational.Builder;
+
+public class Vehicle {
+    private String type;  // Required
+    private int wheels;   // Required
+    private boolean hasAirConditioning; // Optional
+    private boolean hasGPS; // Optional
+
+    // Private constructor to force object creation via Builder
+    private Vehicle(VehicleBuilder builder) {
+        this.type = builder.type;
+        this.wheels = builder.wheels;
+        this.hasAirConditioning = builder.hasAirConditioning;
+        this.hasGPS = builder.hasGPS;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "type='" + type + '\'' +
+                ", wheels=" + wheels +
+                ", hasAirConditioning=" + hasAirConditioning +
+                ", hasGPS=" + hasGPS +
+                '}';
+    }
+
+    public static class VehicleBuilder {
+        private String type;
+        private int wheels;
+        private boolean hasAirConditioning = false; // Default value
+        private boolean hasGPS = false; // Default value
+
+        public VehicleBuilder(String type, int wheels) {
+            this.type = type;
+            this.wheels = wheels;
+        }
+
+        public VehicleBuilder withAirConditioning() {
+            this.hasAirConditioning = true;
+            return this;
+        }
+
+        public VehicleBuilder withGPS() {
+            this.hasGPS = true;
+            return this;
+        }
+
+        public Vehicle build() {
+            return new Vehicle(this);
+        }
+    }
+}
