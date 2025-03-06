a32489d26b8c4bd57dcc3b3f54885ca593022988
Samir Bravo <sambravo@gmail.com>
2025-03-05 19:11:19 -0500

    Bridge Design Pattern

---
+++ src/main/java/designPatterns/structural/Bridge/BridgePatternDemo.java
@@ -0,0 +1,21 @@
+package designPatterns.structural.Bridge;
+
+public class BridgePatternDemo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Test with basic remote");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Test with advanced remote");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
---
+++ src/main/java/designPatterns/structural/Bridge/Remote.java
@@ -0,0 +1,13 @@
+package designPatterns.structural.Bridge;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
